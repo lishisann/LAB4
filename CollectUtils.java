e865be4a0bfded1202b77d4829678b54cf671095
lishisann <and.lyubov31@gmail.com>
2025-11-01 23:33:57 +0500

    Create CollectUtils.java

---
+++ CollectUtils.java
@@ -0,0 +1,16 @@
+package LAB4;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public final class CollectUtils {
+    private CollectUtils() {}
+
+    public static <T, P> P collect(
+            List<T> list,
+            Supplier<P> collectionSupplier,
+            Function<List<T>, P> collectorFunction) {
+        return collectorFunction.apply(list);
+    }
+}
